a8bf01a8a423897a3d08e6c79d4c2b678f5bbfd4
nath-asha <102415462+nath-asha@users.noreply.github.com>
2024-02-28 01:16:25 +0530

    Create loginController.js

---
+++ controllers/loginController.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email, password });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.json({ token });
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
